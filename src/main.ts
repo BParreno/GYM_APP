c1d4977f4529e3039b47c9c1bb84e625ae8bf165
BParreno <biparreno@sudamericano.edu.ec>
2025-06-24 12:10:24 -0500

    Commit 1 FIREBASE
    Implementacion de firebase

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,15 @@ import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalo
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
-    provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideRouter(routes, withPreloading(PreloadAllModules)), provideFirebaseApp(() => initializeApp({ projectId: "gym-app-fire", appId: "1:488766691095:web:d4ce19586f53a4d55f0952", storageBucket: "gym-app-fire.firebasestorage.app", apiKey: "AIzaSyAZe5d-8TJuu_qrnmaDJh6ETVM6szJXNmI", authDomain: "gym-app-fire.firebaseapp.com", messagingSenderId: "488766691095", measurementId: "G-KW4XX759CR" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideStorage(() => getStorage()),
   ],
 });
