2991a9e60a44d221c2b853b55f4b033ef5dba6e7
Pan <mazuniga@sudamericano.edu.ec>
2025-07-17 10:40:22 -0500

    cambios estética y agregar configuración gradel ()

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,23 @@
+// The list of file replacements can be found in angular.json.
+
 export const environment = {
   production: false,
+
   firebaseConfig: {
     projectId: "gym-app-fire",
     appId: "1:488766691095:web:d4ce19586f53a4d55f0952",
-    storageBucket: "gym-app-fire.appspot.com", // Asegúrate de que el formato sea correcto
+    storageBucket: "gym-app-fire.firebasestorage.app",
     apiKey: "AIzaSyAZe5d-8TJuu_qrnmaDJh6ETVM6szJXNmI",
     authDomain: "gym-app-fire.firebaseapp.com",
     messagingSenderId: "488766691095",
     measurementId: "G-KW4XX759CR"
   }
 };
+
+/*
+@@ -13,4 +23,4 @@ export const environment = {
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+// import 'zone.js/plugins/zone-error';   // Included with Angular CLI.
