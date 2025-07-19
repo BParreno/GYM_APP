6c6b54ecb838c007f92bdb346ebbd572e8e17f1b
Pan <mazuniga@sudamericano.edu.ec>
2025-07-19 18:58:26 -0500

    Bug (No se mostraban los labels) arreglado

--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -25,7 +25,9 @@ export class RegisterPage {
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]],
       confirmPassword: ['', [Validators.required]],
+
     }, { validators: this.passwordsMatch });
+
   }
 
   passwordsMatch(form: FormGroup) {
@@ -36,10 +38,8 @@ export class RegisterPage {
 
   async showPrivacyPolicy() {
     const alert = await this.alertController.create({
-      header: 'Políticas y Términos',
-      message: `
-        Somos increibles
-      `,
+      header: 'Añade tu codigo',
+      message: `[    ]`,
       buttons: ['Cerrar']
     });
 
@@ -60,8 +60,6 @@ export class RegisterPage {
     if (this.registerForm.valid) {
       const { username, email, password } = this.registerForm.value;
       console.log('Registrando usuario:', { username, email, password });
-
-      // Aquí iría la lógica real para crear el usuario con Firebase u otro backend
 
       this.router.navigate(['/verify-account']);
     }
